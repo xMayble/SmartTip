3f9bc022f89edd257f09b79eebd093fc1e90e0df
Mehbub Rohit <mehburohit@gmail.com>
2021-01-31 17:30:32 -0500

    Initial Commit

---
+++ SmartTip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartTip
+//
+//  Created by Mehbub Rohit on 1/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
